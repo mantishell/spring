2252fb07bb9a8f4cddde0ea01765892921a67495
mantishell <825013349@qq.com>
2020-04-09 22:29:52 +0800

    基于注解的AOP

---
+++ spring_11_anno_AOP/src/main/java/com/mantishell/service/IAccountService.java
@@ -0,0 +1,6 @@
+package com.mantishell.service;
+
+public interface IAccountService {
+
+    void saveAccount();
+}
---
+++ spring_11_anno_AOP/src/main/java/com/mantishell/service/impl/AccountServiceImpl.java
@@ -0,0 +1,11 @@
+package com.mantishell.service.impl;
+
+import com.mantishell.service.IAccountService;
+import org.springframework.stereotype.Service;
+
+@Service("accountService")
+public class AccountServiceImpl implements IAccountService {
+    public void saveAccount() {
+
+    }
+}
