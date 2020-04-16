664ea6fb43c9fba23bab8d6ee6b3e6b571f996a7
mantishell <825013349@qq.com>
2020-04-16 22:20:47 +0800

    引入事务

---
+++ spring_13_TX/src/main/java/com/mantishell/service/impl/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mantishell.service.impl;
+
+import com.mantishell.dao.IAccountDao;
+import com.mantishell.domain.Account;
+import com.mantishell.service.IAccountService;
+
+public class AccountServiceImpl implements IAccountService {
+
+    private IAccountDao accountDao;
+
+    public void setAccountDao(IAccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+
+    }
+
+    public void transfer(String sourceName, String targetName, Float money) {
+        System.out.println("transfer....");
+        //2.1根据名称查询转出账户
+        Account source = accountDao.findAccountByName(sourceName);
+        //2.2根据名称查询转入账户
+        Account target = accountDao.findAccountByName(targetName);
+        //2.3转出账户减钱
+        source.setMoney(source.getMoney()-money);
+        //2.4转入账户加钱
+        target.setMoney(target.getMoney()+money);
+        //2.5更新转出账户
+        accountDao.updateAccount(source);
+
+        int i=1/0;
+
+        //2.6更新转入账户
+        accountDao.updateAccount(target);
+    }
+}
